250694123923bc9c8301d2f5b18cc00587ba9dc3
tonyprots <tony-prots@yandex.ru>
2019-06-28 23:58:55 +0300

    sum matrix solved

--- Week 2/Sum matrix.py
+++ Week 2/Sum matrix.py
@@ -8,8 +8,43 @@
 В случае одной строки/столбца элемент сам себе является соседом по соответствующему направлению.
 """
 
-x=[][]
-count=0
-while x!="end":
-    x[count][]=input().split()
-print(x)
+count = 0
+x = []
+end = ["end"]
+while True:
+    x.append([])
+    x[count] = (input().split())
+    count += 1
+    if x[count - 1] == end:
+        x.pop()
+        break
+y = []
+stolbtsy = len(x[0])
+stroki = len(x)
+for i in range(stroki):
+    y.append([])
+    for j in range(stolbtsy):
+
+        a = i - 1
+        c = i + 1
+
+        b = j - 1
+        d = j + 1
+
+        if i == 0:
+            a = stroki - 1
+        if j == 0:
+            b = stolbtsy - 1
+
+        if i == stroki - 1:
+            c = 0
+        if j == stolbtsy - 1:
+            d = 0
+        y[i].append(int(x[a][j]) + int(x[c][j]) + int(x[i][b]) + int(x[i][d]))
+
+for i in range(len(y)):
+    for j in range(len(y[0])):
+        if j!=len(y[0])-1:
+            print(y[i][j], end=" ")
+        else: print(y[i][j], end="\n")
+
